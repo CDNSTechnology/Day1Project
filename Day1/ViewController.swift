1286235b2547c115ee2d9ce713b6ec4c1192907e
MacBook <macbook@Akash.local>
2019-01-03 11:56:22 +0530

    Initial

---
+++ Day1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day1
+//
+//  Created by MacBook on 1/3/19.
+//  Copyright © 2019 CDNS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
